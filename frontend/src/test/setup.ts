30f3b124c3da344dbe388f2e11a37455e13b717b
J28u <judithcecchin@gmail.com>
2026-06-03 11:37:00 +0200

    :white_check_mark: Add unit tests for Navbar component (display, logout)

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,6 +1,7 @@
 import { beforeAll, afterEach, afterAll } from "vitest";
 import { server } from "./mock/server";
 import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 
