3b42bc69f3bebcb3eb2ff3f92a9c7af1c11c2645
J28u <judithcecchin@gmail.com>
2026-06-11 16:48:41 +0200

    :recycle: Use toUserResponse function to simplify user service getById

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -22,15 +22,7 @@ export class UserService {
     const user = await this.userRepository.findOne(id);
     if (!user) throw new NotFoundError("User not found");
 
-    return {
-      id: user.id,
-      email: user.email,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      admin: user.admin,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-    };
+    return this.toUserResponse(user);
   }
 
   async delete(userId: number, id: number): Promise<void> {
